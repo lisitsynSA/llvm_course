56de1b65c0f583fa0d41bc339233a8f2dca65dd2
Lisitsyn Sergey <fox9508@ya.ru>
2024-04-09 08:54:24 -0400

    [Full] Add full pipeline example for LLVM course

---
+++ FullPipeline/compile_sim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd $(dirname "$0")
+set -x
+
+cat $1.lang | ../Flex_Bison/lex12 > $1.ll || exit 1
+opt $1.ll -O2 -o $1.opt.ll -S  || exit 1
+../../llvm-project/build/bin/llc $1.opt.ll -march sim --filetype=asm  || exit 1
+../../llvm-project/build/bin/llc $1.opt.ll -march sim --filetype=obj  || exit 1
+../Simulator/sim $1.opt.o
